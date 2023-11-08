c506514cb10483bdd90d3caa8b8ee7e45d522e49
Your Name <you@example.com>
2023-11-07 23:15:44 -0800

    need to add database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, flash, render_template, session, url_for, redirect, request
+from flask import Flask, flash, render_template, session, url_for, redirect, request, jsonify
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -9,65 +10,83 @@ import string
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 
-from branchLogic import sentiment_analysis, pre_processing
-
+from branchLogic import sentiment_analysis, pre_processing, response, link_resource, yes, res_img, resource_name
 app = Flask(__name__)
 app.secret_key = "bello"
 
-  
-logic = {1: "Hello! My name is Codi. What is your name?",
-         2: "How could I be of assistance? Please give me a 3-5 sentence explanation of your current situation so I can get a better understanding.",
-         3: "TEST",
-         }
-log_i = 1
-person = {
-  "problem": []
 
-}
-quest ="ppp"
+logic = ["How long have you been feeling like this? ", "Who have you talked to about this issue?", "test"] 
+        
 
+person = {"user" : "null", "user_type" :"null", "gradelvl" : 0, "message" : "null", "problem" : "schoolwork"}
 
 @app.route('/', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["name"] 
+        user_type = request.form["user_type"]
+
+        person = {"user": user, "user_type" : user_type }
+        print(person)
+        if user_type == "Admin":
+            return redirect(url_for("admin"))
+        elif user_type == "Student":
+            return redirect(url_for("index"))
+    else:
+        return render_template('login.html')
+
+@app.route('/admin', methods=["POST", "GET"])
+def admin():
+    p = person
+    return render_template("admin.html", person=p)
+
+@app.route('/index', methods=["POST", "GET"])
 def index():
-    global log_i
-    content = logic[log_i + 1]
+    content =  "How could I be of assistance? Please give me a 3-5 sentence explanation of your current situation so I can get a better understanding."
     if request.method == "POST":
         message = request.form["message"]
         message = pre_processing(message)
 
-        user = request.form["name"]
-        age = request.form["age"]
-
-        person = {"user": user, "age": age, "message": message, "problem" : sentiment_analysis(message)}
-        print(person)
-        return redirect(url_for("questionare", quest=logic[3]))
+        person = { "message": message, "problem" : sentiment_analysis(message)}
+        global results
+        results = "I'm really sorry to hear that you're feeling this way, and I want you to know that you're not alone. It's completely normal to face challenges in school, and it's okay to ask for help. You have a lot of potential, and I believe in your ability to overcome this. Together, we can work on finding solutions that will make things better for you. Remember, progress is a journey, and I'm here to support you every step of the way."
+    
+        return redirect(url_for("res", result=results))
+        # return redirect(url_for("questionare", quest=logic[1]))
     else:
         return render_template('index.html', content=content)
+    
+results = "I'm really sorry to hear that you're feeling this way, and I want you to know that you're not alone. It's completely normal to face challenges in school, and it's okay to ask for help. You have a lot of potential, and I believe in your ability to overcome this. Together, we can work on finding solutions that will make things better for you. Remember, progress is a journey, and I'm here to support you every step of the way."
+@app.route("/res", methods=["GET", "POST"])
+def res():
+   
+    if request.method == "POST":
+        return redirect(url_for("questionare", quest=logic[0]))
+    else:
+        return render_template("results.html", results=results)
+    
+
+
+resources = link_resource(person["problem"])
+resource_img = res_img[person["problem"]]
+final_res = yes[person["problem"]]
+nm = resource_name[person["problem"]]
 
 @app.route("/questionare", methods=["GET", "POST"])
-def questionare():
-    if request.method == "POST":
-        log_i += 1
-        ans = request.form["ans"]
-        print(ans)
-        print(log_i)
-    return render_template('questionare.html', quest=logic[3])
+def questionare():  
+    if request.method == "POST":    
+        ans1 = request.form["inputs1"]
+        ans2 = request.form["inputs2"]
+            
+        return render_template("finalres.html",  resources=resources, img=resource_img, name=nm,fr= final_res)
+    else:
+        return render_template('questionare.html')
 
 
-@app.route("/user")
-def user():
-  if "user" in session:
-    user = session["user"]
-    return f"<h1>{user}</h1>"
-  else: 
-    if "user" in session:
-      return redirect(url_for("user"))
-    return redirect(url_for("login"))
 
-@app.route("/logout")
-def logout():
-  session.pop("user", None)
-  return render_template('login.html')
-
+@app.route("/final_results")    
+def final_results():
+    return render_template("finalres.html",  resources=resources, img=resource_img, name=nm,fr= final_res)
+    
 if __name__ == "__main__":
-  app.run(debug=True, port=8001) 
+    app.run(debug=True, port=5000) 
--- branchLogic.py
+++ branchLogic.py
@@ -24,13 +24,60 @@ def sentiment_analysis(str):
             return "divorce"
         elif ("argument" or "argument with parents" or "argument with family" or "arguing") in str:
             return"parental argument"
-        elif ("money" or "financial" or "finance" or "cash"):
+        elif ("money" or "financial" or "finance" or "cash") in str:
              return "finacial" 
+        elif ("grades" or "tests" or "assignments" or "homework") in str:
+             return "schoolwork" 
         else:
-            print('false')
+            print('Can you be a little more specific?')
 
-# def followup_quest:
+resp = {
+    
+     "divorce" : "I'm really sorry to hear that you're feeling this way, and I want you to know that you're not alone. It's completely normal to face challenges in school, and it's okay to ask for help. You have a lot of potential, and I believe in your ability to overcome this. Together, we can work on finding solutions that will make things better for you. Remember, progress is a journey, and I'm here to support you every step of the way.",
+     "parental_argument" : "",
+     "finacial" : "",
+     "schoolwork" : "grade test 2",
+    }
 
+def response(str):
+     if ("divorce") in str:
+          return resp["divorce"]
+     if ("parental_argument"):
+          return resp["parental_argument"]
+     if ("finacial") in str:
+          return resp["finacial"]
+     if("schoolwork") in str:
+          return resp["schoolwork"]
+     
+resources = {  "divorce" : "https://www.library.ca.gov/services/to-libraries/online-tutoring/#:~:text=The%20California%20State%20Library%20provides,using%20state%20curriculum%20and%20standards.",
+     "parental_argument" : "",
+     "finacial" : "",
+     "schoolwork" : "grades test",
+}
 
+resource_name = {
+      "schoolwork": "California State Library Free tutoring program",
+}
+res_img = {
+      "divorce" : "src/brainfuse-gethomeworkhelp@600.jpg",
+     "parental_argument" : "",
+     "finacial" : "",
+     "schoolwork" : "src/brainfuse-gethomeworkhelp@600.jpg",
+}
 
-# print(processed_situation)
+yes = {
+      "divorce" : "try talk to trusted friends and family about this issues. speaking with others is a sign of strength and they would be more than happy to help you",
+     "parental_argument" : "",
+     "finacial" : "",
+     "schoolwork" : "try talk to trusted friends and family about this issues. speaking with others is a sign of strength and they would be more than happy to help you",
+}
+
+def link_resource(str):
+     if ("divorce") in str:
+          return resources["divorce"]
+     if ("parental_argument"):
+          return resources["parental_argument"]
+     if ("finacial") in str:
+          return resources["finacial"]
+     if("schoolwork") in str:
+          return resources["schoolwork"]
